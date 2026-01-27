e9f267abe47ed3c5c3583ec88f185befc827ed06
wylited <44154103+wylited@users.noreply.github.com>
2026-01-27 15:50:02 +0000

    feat(vpn): bg+connection status

--- practice/connect.sh
+++ practice/connect.sh
@@ -1,1 +1,4 @@
-sudo openvpn --config hkcertctf-team11.ovpn --tun-mtu-max 64800
+echo "connecting to hkcert team 11 in the background"
+
+sudo openvpn --config hkcertctf-team11.ovpn --tun-mtu-max 64800 --daemon
+ip route show dev tun0
